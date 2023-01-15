d530d2f8b94868e59e9625c6f98aceceddecc2e6
codeico <bangico@students.amikom.ac.id>
2023-01-15 12:51:04 +0700

    Portoku

---
+++ js/_buildManifest.js
@@ -0,0 +1,2 @@
+
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/886-29088ebf4226abf1.js","static/chunks/pages/index-f3da067f10cc2260.js"],"/_error":["static/chunks/pages/_error-c0033c2fc08817db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
